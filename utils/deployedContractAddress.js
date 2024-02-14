084b1d14f59145665065b56f5bbda0c182026435
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2024-02-14 17:33:17 +0200

    abis

---
+++ utils/deployedContractAddress.js
@@ -0,0 +1,31 @@
+const PionerV1UtilsAddress = "0x4cFDc54985B04C944f6Ec7192873473acB5d4F92";
+const FakeUSDAddress = "0xF1FBfB0a2Cc4c44253b39542B26716314A34F0B6";
+const PionerV1Address = "0xbfd48b8255FE04615C441EAa7033C145f432A2d0";
+const PionerV1ComplianceAddress = "0x13DDdc7BfE3bdDCde46249D86C1a23EDd5C3212E";
+const PionerV1OpenAddress = "0xEf1C0Bc9506BF71A9f489E7f7B559A5DC957362D";
+const PionerV1CloseAddress = "0xafe3F9845E1023Aa71BE74E9DDCf29402c95Dc25";
+const PionerV1DefaultAddress = "0x4dE711a0aD4e91637F66d0977a79Ce59F0A30B27";
+const PionerV1StableAddress = "0xB10FFAa9e4facbC3080612b178938431fA7aD9a0";
+const PionerV1ViewAddress = "0x4Eec7C251fc99951B33B8ddB2cB0922f438EF8f2";
+const PionerV1OracleAddress = "0xD607412293711928C574a5CcF8AF2e1aca0d4543";
+const PionerV1WarperAddress = "0x6970b2edc9127660107F0bf45302b26324eD144e";
+
+path = [
+    ".\abis\PionerV1CA.sol\PionerV1CA.json",
+    ".\abis\PionerV1Close.sol\PionerV1Close.json",
+    ".\abis\PionerV1Compliance.sol\PionerV1Compliance.json",
+    ".\abis\PionerV1Default.sol\PionerV1Default.json",
+    ".\abis\PionerV1Open.sol\PionerV1Open.json",
+    ".\abis\PionerV1Stable.sol\PionerV1ERC20.json",
+    ".\abis\PionerV1Stable.sol\PionerV1Stable.json",
+    ".\abis\PionerV1Storage.sol\PionerV1Storage.json",
+    ".\abis\PionerV1Warper.sol\PionerV1Warper.json",
+    ".\abis\PionerV1Oracle.sol\PionerV1Oracle.json",
+    ".\abis\Libs\PionerV1Utils.sol\PionerV1Utils.json",
+    ".\abis\Libs\PionerV1View.sol\PionerV1View.json",
+    ".\abis\Libs\SchnorrSECP256K1Verifier.sol\SchnorrSECP256K1Verifier.json",
+    ".\abis\Libs\SchnorrSECP256K1VerifierV2.sol\SchnorrSECP256K1VerifierV2.json",
+    ".\abis\Libs\MuonClientBase.sol\MuonClientBase.json",
+    ".\abis\PionerV1.sol\PionerV1.json",
+    ".\abis\FakeUSD.sol\fakeUSD.json",
+    ];
